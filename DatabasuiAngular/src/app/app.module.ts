0cbde1644472e31ef5fb9848cc92c920cb8ed217
Stephen Negron <negstephen@gmail.com>
2018-07-08 21:45:28 -0400

    added sidenav, table, and download components

--- DatabasuiAngular/src/app/app.module.ts
+++ DatabasuiAngular/src/app/app.module.ts
@@ -9,6 +9,46 @@ import { ModalComponent } from './components/modal/modal.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ConsoleRoutingModule } from './components/data-console/console-routing.module';
 import { TableComponent } from './components/table/table.component';
+import { DownloadComponent } from './components/download/download.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -17,12 +57,51 @@ import { TableComponent } from './components/table/table.component';
     NavbarComponent,
     DataConsoleComponent,
     ModalComponent,
-    TableComponent
+    TableComponent,
+    DownloadComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ConsoleRoutingModule
+  ],
+  exports: [
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
